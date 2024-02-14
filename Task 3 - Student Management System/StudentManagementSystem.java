fe30111fb159ab4f4fac975042bbcd64de0bccc7
SanthosN8 <107839565+SanthosN8@users.noreply.github.com>
2024-02-14 10:11:06 +0530

    Update StudentManagementSystem.java

--- Task 3 - Student Management System/StudentManagementSystem.java
+++ Task 3 - Student Management System/StudentManagementSystem.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class SManagement {
+public class StudentManagementSystem {
 
     public static void main(String[] args){
         Scanner obj = new Scanner(System.in);
